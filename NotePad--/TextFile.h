9b3ac465031c2b8bc4309f1cbbd1f600b98e3b9b
Jamillll <mjmister2006@outlook.com>
2024-04-23 00:16:08 +0100

    Turned TextFile class into seperate header and source files

--- NotePad--/TextFile.h
+++ NotePad--/TextFile.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <fstream>
-#include <stdio.h>
 #include <string>
 
 class TextFile
@@ -9,34 +8,9 @@ private:
 	std::string m_Path;
 
 public:
-	TextFile(std::string inputPath)
-	{
-		m_Path = inputPath;
-	}
+	TextFile(std::string inputPath);
 
-	std::string getContent()
-	{
-		std::fstream file(m_Path);
+	std::string GetContent();
 
-		std::string content;
-		std::string currentLine;
-
-		while (std::getline(file, currentLine))
-		{
-			content += currentLine + "\n";
-		}
-
-		file.close();
-		return content;
-	}
-
-	void commitContent(std::string content)
-	{
-		const char* charPath = m_Path.c_str();
-		remove(charPath);
-
-		std::ofstream file(charPath);
-		file << content;
-		file.close();
-	}
+	void CommitContent(std::string content);
 };
