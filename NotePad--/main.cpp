2eadd8a57ac1d3c3e1f4256b482259e50d7b86a5
Jamillll <mjmister2006@outlook.com>
2024-04-23 15:05:58 +0100

    Added MultiViewports
    Enabled multi viewports imGui feature

--- NotePad--/main.cpp
+++ NotePad--/main.cpp
@@ -30,14 +30,16 @@ int main(int argc, char* argv[])
     ImGuiIO& io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
     ImGui::StyleColorsDark();
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
 
-    bool showDemoWindow = true;
+    bool showDemoWindow = false;
     while (!glfwWindowShouldClose(window))
     {
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwPollEvents();
 
         // Start the Dear ImGui frame
@@ -45,11 +47,31 @@ int main(int argc, char* argv[])
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::ShowDemoWindow(&showDemoWindow);
+        if (showDemoWindow)
+            ImGui::ShowDemoWindow(&showDemoWindow);
 
+        ImGui::Begin("Notepad--");
+        ImGui::Text("Hello World");
+        if (ImGui::Button("Show Demo Window"))
+        {
+            showDemoWindow = true;
+        }
+        ImGui::End();
+
+        // Rendering
         ImGui::Render();
-        glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // Update and Render additional Platform Windows
+        // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+        //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            GLFWwindow* backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
 
         glfwSwapBuffers(window);
     }
